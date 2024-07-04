a05c2e51f406085d144e2f0a3834ac4e443495ad
schmeekygeek <ss6087982@gmail.com>
2024-07-04 17:36:13 +0530

    initial commit

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net"
+  "log"
+	"net/http"
+  "github.com/gobwas/ws"
+)
+
+type Server struct {
+  rooms map[string]Room
+}
+
+type Room struct {
+  clients[] Client
+  textState []byte
+}
+
+type Client struct {
+  conn      *net.Conn
+  username  string
+  cursor    CursorPosition
+}
+
+type CursorPosition struct {
+  x, y int
+}
+
+func (s *Server) Serve(w http.ResponseWriter, r *http.Request) {
+  conn, _, _, err := ws.UpgradeHTTP(r, w)
+  log.Printf("%v connected", conn.RemoteAddr().String())
+  if err != nil {
+    log.Println(err.Error())
+  }
+}
+
+func Init() *Server {
+  s := Server{
+  	rooms: map[string]Room{},
+  }
+  return &s
+}
