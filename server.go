b34feda1913044ea49928ceb2861dcaa14b0cdfa
schmeekygeek <ss6087982@gmail.com>
2024-07-06 20:35:04 +0530

    check url params for room and basic comms

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net"
-  "log"
 	"net/http"
-  "github.com/gobwas/ws"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 )
 
 type Server struct {
@@ -26,10 +28,36 @@ type CursorPosition struct {
   x, y int
 }
 
+type Message struct {
+
+}
+
 func (s *Server) Serve(w http.ResponseWriter, r *http.Request) {
   // get query parameter
   conn, _, _, err := ws.UpgradeHTTP(r, w)
+  roomId := r.URL.Query().Get("room")
+  if roomId == "" {
+    roomId = "1234"
+    // create a new room
+  }
+  room := s.rooms[roomId]
+  room.clients = append(room.clients, Client{
+  	conn:     &conn,
+  	username: "something",
+  	cursor:   CursorPosition{
+  		x: 0,
+  		y: 0,
+  	},
+  })
   log.Printf("%v connected", conn.RemoteAddr().String())
+  for {
+    msg, _, err := wsutil.ReadClientData(conn)
+    log.Printf("%v said: %v", conn.RemoteAddr(), string(msg))
+    if err != nil {
+      log.Println(err)
+      break
+    }
+  }
   if err != nil {
     log.Println(err.Error())
   }
